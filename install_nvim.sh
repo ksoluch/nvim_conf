1b9d0b32417b1b93a1f941ca8871abb408af5df9
Krzysiek <ksoluch@gmail.com>
2018-06-10 11:56:14 +0200

    nvim installation sh

---
+++ install_nvim.sh
@@ -0,0 +1,21 @@
+apt-get install software-properties-common
+apt-add-repository ppa:neovim-ppa/stable
+apt-get update
+
+apt-get install neovim
+
+apt-get install python-dev python-pip python3-dev python3-pi
+pip2 install --upgrade neovim
+pip3 install --upgrade neovim
+
+apt-get install ruby2.3 ruby2.3-dev
+gem install neovim
+
+apt-get install clang-format ssh xclip g++ gcc
+apt-get install exuberant-ctags cscope tmux
+
+mkdir ~/.vim
+
+#mkdir ~/.config/nvic
+#git clone https://github.com/ksoluch/nvim_conf.git ~/.config/nvim/
+
